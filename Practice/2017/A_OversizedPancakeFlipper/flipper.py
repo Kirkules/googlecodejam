c5ffba75411c8c8ee77fbb6b985985bc9073804b
Kirk Boyer <kirboyer@cs.du.edu>
2018-03-22 16:51:53 -0600

    add Practice folder; add started problem A, 2017

---
+++ Practice/2017/A_OversizedPancakeFlipper/flipper.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+# reading input
+def parse_input(filename):
+    with open(filename, "r") as f:
+        test_cases = [case.split(" ") for case in f.readlines()[1:]]
+        test_cases = [(pancakes, int(k)) for pancakes, k in case
+                      for case in test_cases]
+
+    return test_cases
+
+
+def flip(string, k, i):
+    return (string[:i] +
+            "".join("+" if char == "-" else "-" for
+                    char in string[i:(i+k)]) +
+            string[(i+k):])
+
+
+# actually solve problem
+def solve(pancakes, k):
+    n = len(pancakes)
+    # Lemma: if an endpoint pancake p is '-', it must be flipped exactly once.
+    # pf: if it's '-' it needs flipped an odd number of times, and there is
+    # exactly one window (0 through k-1)
+
+
+# make this file a runnable script
+if __name__ == "__main__":
+    from sys import argv as arguments
+    from os import path
+
+    if arguments[1] == "-single":
+        pancakes = arguments[2]
+        k = int(arguments[3])
+        result = solve(pancakes, k)
+        print("result for {0}, {1}: {2}".format(pancakes, k, result))
+    else:
+        infname = arguments[1]
+        outfname = path.splitext(infname)[0] + ".out"
+
+        test_cases = parse_input(arguments[1])
+        results = [solve(case) for case in test_cases]
+        with open(outfname, "w") as f:
+            f.write("\n".join(
+                "Case #{index}: {result}".format(index=i+1, result=r) for
+                i, r in enumerate(results)))
+
+        print("Done processing {0}. Check {1} for results.".format(
+            infname, outfname))
